50c8c203e912e552a91afa4b686d47bb60b093fc
KingAusti <austin@thehenrys.xyz>
2022-06-01 23:43:02 -0700

    reconfiguring bundle

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -1,5 +1,6 @@
 require("bootstrap");
 const createEl = require("./domMethods");
+const { createLoremIpsum, dateConverter } = require("./helpers");
 
 if (window.location.href.indexOf("schedule") > -1) {
 
